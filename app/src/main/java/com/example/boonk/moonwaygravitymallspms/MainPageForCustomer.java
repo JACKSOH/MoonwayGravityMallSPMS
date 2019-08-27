511fff26f7c6a0ac6277f582edb480ab637acc2d
GORDON9839 <gohcl-aa16@student.tarc.edu.my>
2019-08-27 17:15:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/boonk/moonwaygravitymallspms/MainPageForCustomer.java
@@ -0,0 +1,13 @@
+package com.example.boonk.moonwaygravitymallspms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainPageForCustomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page_for_customer);
+    }
+}
